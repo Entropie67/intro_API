fb5e4f8a556abee8db19a37398e42023eeba89c4
Entropie67 <olivier.elophe@gmail.com>
2024-01-30 09:04:33 +0100

    affichage de deux personnages sur la pages avec des données en local

--- script.js
+++ script.js
@@ -2,5 +2,20 @@
 // réccupère la balise d'id result
 result = document.querySelector('#result');
 
+// Travail sur l'API
 
-result.innerHTML += " Réponse";
+// On fait un test avec des données locales
+let personnages = [
+                                            {"id": 1, "name": "Henri"},
+                                            {"id": 2, "name": "Charlotte"}
+                                            ];
+
+
+
+// Ajoutons les personnages sur la page :
+for (perso of personnages){
+    // += plutôt que = pour ajouter les personnages les uns à la suite des autres
+    result.innerHTML += ` <p>Personnage : ${perso.name}</p>`; // les " " sont ceux du 7 !! ` `
+}
+
+
